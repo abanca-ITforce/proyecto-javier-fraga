52ddca5a5ce3e9a2840595c2c5271582d560bd51
javier-fraga <javierfraga1997@gmail.com>
2019-12-02 17:27:01 +0100

    lazy home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomeModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
